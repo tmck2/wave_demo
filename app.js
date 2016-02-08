55c32052e375fbd216e54223091178dd6ff523de
Anthony McKinney <anthonymckinney@Anthonys-iMac.local>
2016-02-07 22:30:07 -0500

    Replace div view with canvas view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require(['observable', 'slider', 'utilities'], function(observable, slider, utilities) {
 	var PI = 3.14159265359
-	var NUM_SAMPLES = 300
+	var NUM_SAMPLES = 800
 
 	function wave() {
 		this.a = 0.25
@@ -38,7 +38,8 @@ require(['observable', 'slider', 'utilities'], function(observable, slider, util
 
 		self.model = new wave()
 		utilities.extend(self.model, new observable())
-		self.view = new view(self.model)
+		
+		//self.view = new view(self.model)
 
 		self.sliderContainer = document.getElementById('sliders')
 		self.aslider = new slider.controller(self.sliderContainer)
@@ -102,8 +103,11 @@ require(['observable', 'slider', 'utilities'], function(observable, slider, util
 	function canvasView(model) {
 		var self = this
 		var canvas = document.getElementById('canvas')
+		this.height = 600
+		this.width = 800
+		canvas.width = this.width
+		canvas.height = this.height
 		this.ctx = canvas.getContext('2d')
-
 		model.addObserver({update: function() { render() }})
 		render()
 
@@ -113,21 +117,22 @@ require(['observable', 'slider', 'utilities'], function(observable, slider, util
 
 		function render() {
 			var samples = model.getSamples()
-			points = samples.map(function(pt) { return [pt[0], pt[1]*75+75] })
-			self.ctx.clearRect(0,0,300,150)
-			self.ctx.fillStyle = "green"
+			points = samples.map(function(pt) { return [pt[0], pt[1]*(self.height/2)+(self.height/2)] })
+			self.ctx.fillStyle = "#55205E"
+			self.ctx.fillRect(0,0,self.width,self.height)
+			var grad = self.ctx.createLinearGradient(0,0,0,self.height);
+			grad.addColorStop(0,"#0082AD")
+			grad.addColorStop(1,"#00BFFF")
+			self.ctx.fillStyle = grad
 			self.ctx.beginPath()
 			self.ctx.moveTo(points[0][0], points[0][1])
 			for(i=1; i<points.length; i++)
 			{
 				self.ctx.lineTo(points[i][0], points[i][1])
 			}
-			self.ctx.lineTo(300, points[299][1])
-			self.ctx.strokeStyle = "indigo"
-			self.ctx.lineWidth = 5
-			self.ctx.stroke()
-			self.ctx.lineTo(300, 150)
-			self.ctx.lineTo(0, 150)
+			self.ctx.lineTo(self.width, points[299][1])
+			self.ctx.lineTo(self.width, self.height)
+			self.ctx.lineTo(0, self.height)
 			self.ctx.fill()
 		}
 	}
